b4f8551e5cdde600df44f6ac18eaa49201cd0d4c
Jishnu Setia <jishnusetia8@gmail.com>
2023-09-08 22:48:35 +0400

    Commit 3

--- Game.java
+++ Game.java
@@ -10,13 +10,17 @@ public class Game{
     Container con;
     Color mainColor = new Color(0,78,143);//128,222,255
     Font font = new Font("Avenir", Font.PLAIN, 60);
+    
+    //Borders
+    JPanel side1Panel, side2Panel, topPanel, bottomPanel;
+    JLabel side1, side2, top, bottom;
 
     //title screen
-    JPanel titleImagePanel1, titleTextPanel, titleImagePanel2, startButtonPanel, exitButtonPanel,howToPlayPanel;
-    JLabel titleImageLabel1, titleTextLabel, titleImageLabel2;
-    JButton startButton, exitButton, howToPlayButton;
+    JPanel titleImagePanel, whiteLine, startButtonPanel, exitButtonPanel,howToPlayPanel, creditsPanel;
+    JLabel titleImageLabel;
+    JButton startButton, exitButton, howToPlayButton, creditsButton;
 
-    //how to play screen
+    //How to Play Screen
 
     //Players Screen
 
@@ -35,35 +39,23 @@ public class Game{
         titleScreen();
     }
     private void titleScreen(){
-        titleImagePanel2 = new JPanel();
-        titleImagePanel2.setBounds(50, 20, 100, 100);
-        titleImagePanel2.setBackground(mainColor);
-        titleImageLabel2 = new JLabel();
-        ImageIcon icon1 = resizeImage("res/logoV2.png", 100, 100);
-        titleImageLabel2.setIcon(icon1);
-        titleImagePanel2.add(titleImageLabel2);
-        con.add(titleImagePanel2);
+        //content coordinates range from (140, 170) to (750, 500)
+        titleImagePanel = new JPanel();
+        titleImagePanel.setBounds(140, 170, 400, 400);
+        titleImagePanel.setBackground(mainColor);
+        titleImageLabel = new JLabel();
+        ImageIcon icon = resizeImage("res/logoWText.png", 400, 400);
+        titleImageLabel.setIcon(icon);
+        titleImagePanel.add(titleImageLabel);
+        con.add(titleImagePanel);
 
-        titleTextPanel = new JPanel();
-        titleTextPanel.setBounds(150, 30, 700, 150);
-        titleTextPanel.setBackground(mainColor);
-        titleTextLabel= new JLabel("The Alternative Alphabet");
-        titleTextLabel.setFont(font);
-        titleTextLabel.setForeground(new Color(40,171,255));//new Color(103,46,105)
-        titleTextPanel.add(titleTextLabel);
-        con.add(titleTextPanel);
-
-        titleImagePanel1 = new JPanel();
-        titleImagePanel1.setBounds(50, 150, 800, 300);
-        titleImagePanel1.setBackground(mainColor);
-        titleImageLabel1 = new JLabel();
-        ImageIcon icon = resizeImage("res/head.png", 800, 285);
-        titleImageLabel1.setIcon(icon);
-        titleImagePanel1.add(titleImageLabel1);
-        con.add(titleImagePanel1);
+        whiteLine = new JPanel();
+        whiteLine.setBounds(540, 170, 1, 400);
+        whiteLine.setBackground(Color.white);
+        con.add(whiteLine);
 
         startButtonPanel = new JPanel();
-        startButtonPanel.setBounds(50, 450, 800, 60);
+        startButtonPanel.setBounds(550, 220, 200, 70);
         startButtonPanel.setBackground(mainColor);
         startButton = new JButton("Start");
         startButton.setFont(font.deriveFont(35f));
@@ -76,7 +68,7 @@ public class Game{
         con.add(startButtonPanel);
 
         howToPlayPanel = new JPanel();
-        howToPlayPanel.setBounds(50, 520, 800, 60);
+        howToPlayPanel.setBounds(545, 300, 325, 70);
         howToPlayPanel.setBackground(mainColor);
         howToPlayButton = new JButton("How to Play");
         howToPlayButton.setFont(font.deriveFont(35f));
@@ -88,8 +80,21 @@ public class Game{
         howToPlayPanel.add(howToPlayButton);
         con.add(howToPlayPanel);
 
+        creditsPanel = new JPanel();
+        creditsPanel.setBackground(mainColor);
+        creditsPanel.setBounds(570, 380, 200, 70);
+        creditsButton = new JButton("Credits");
+        creditsButton.setFont(font.deriveFont(35f));
+        creditsButton.setForeground(mainColor);
+        creditsButton.setBackground(Color.white);
+        creditsButton.addActionListener((ActionEvent e) -> {
+            System.out.println("Credits");
+        });
+        creditsPanel.add(creditsButton);
+        con.add(creditsPanel);
+
         exitButtonPanel = new JPanel();
-        exitButtonPanel.setBounds(50, 590, 800, 60);
+        exitButtonPanel.setBounds(545, 460, 200, 70);
         exitButtonPanel.setBackground(mainColor);
         exitButton = new JButton("Exit");
         exitButton.setFont(font.deriveFont(35f));
@@ -104,21 +109,49 @@ public class Game{
     private void createWindow(){
         window = new JFrame("Alternative Alphabet");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(900, 800);
+        window.setSize(1000, 800);
         window.setLocationRelativeTo(null);
         window.getContentPane().setBackground(mainColor);
         window.setBackground(mainColor);
         window.setLayout(null);
         window.setResizable(false);
         con = window.getContentPane();
-    }
-    private void hide(){
-        titleImagePanel1.setVisible(false);
-        titleTextPanel.setVisible(false);
-        titleImagePanel2.setVisible(false);
-        startButtonPanel.setVisible(false);
-        exitButtonPanel.setVisible(false);
-        howToPlayPanel.setVisible(false);
+
+        side1Panel = new JPanel();
+        side1Panel.setBounds(-10, 0, 150, 800);
+        side1Panel.setBackground(mainColor);
+        side1 = new JLabel();
+        ImageIcon icon = resizeImage("res/sidedesign.png", 141, 750);
+        side1.setIcon(icon);
+        side1Panel.add(side1);
+        con.add(side1Panel);
+
+        topPanel = new JPanel();
+        topPanel.setBounds(141, 0, 700, 170);
+        topPanel.setBackground(mainColor);
+        top=new JLabel();
+        icon = resizeImage("res/horizontalDesign.png", 600, 146);
+        top.setIcon(icon);
+        topPanel.add(top);
+        con.add(topPanel);
+
+        bottomPanel = new JPanel();
+        bottomPanel.setBounds(141, 600, 700, 170);
+        bottomPanel.setBackground(mainColor);
+        bottom=new JLabel();
+        icon = resizeImage("res/horizontalDesign.png", 600, 146);
+        bottom.setIcon(icon);
+        bottomPanel.add(bottom);
+        con.add(bottomPanel);
+
+        side2Panel = new JPanel();
+        side2Panel.setBounds(850, 0, 150, 800);
+        side2Panel.setBackground(mainColor);
+        side2 = new JLabel();
+        icon = resizeImage("res/sidedesign.png", 141, 750);
+        side2.setIcon(icon);
+        side2Panel.add(side2);
+        con.add(side2Panel);
     }
     private ImageIcon resizeImage(String imagePath, int width, int height) {
         try {
